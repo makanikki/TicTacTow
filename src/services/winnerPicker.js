a1e98684bef1880d860b03a1dced7c0ce8d8b9f3
Mathieu ROBERT <mathieu@rbtmathieu.fr>
2019-04-10 11:55:04 +0200

    Pick winner & misc

--- src/services/winnerPicker.js
+++ src/services/winnerPicker.js
@@ -3,7 +3,7 @@ export function buildMatrice(board) {
 
   return Object.keys(board).reduce((acc, cellId) => {
     const cell = board[cellId];
-    const cellRow = Math.ceil(cellId / sideLength) - 1;
+    const cellRow = Math.floor(cellId / sideLength);
 
     if (!acc[cellRow]) acc[cellRow] = [];
     acc[cellRow].push(cell);
@@ -14,27 +14,25 @@ export function buildMatrice(board) {
 
 export function getNewCoords(currentCoords, direction) {
   return currentCoords.map(
-    (coord, i) => parseInt(coord) + parseInt(direction[i])
+    (coord, i) => parseInt(coord) + parseInt(direction[i]),
   );
 }
 
+// TODO: Only return a single line. Return format : ['0,0_1,1', 'X']
 export function findWinPattern(lines) {
   return Object.keys(lines).reduce((acc, lineId) => {
+    if (acc) return acc;
     const line = lines[lineId];
-    const isWin = line.every(item => item === line[0]);
+    const isWin = line.every(item => item !== undefined && item === line[0]);
 
-    return isWin
-      ? {
-          ...acc,
-          [lineId]: line
-        }
-      : acc;
+    return isWin ? [lineId, line[0]] : acc;
   }, undefined);
 }
 
 export function exploreVector(matrice, currentCoords, direction, chain = []) {
   const [x, y] = currentCoords;
-  const newChain = [...chain, matrice[x][y]];
+  // console.warn("x, y", x, y);
+  const newChain = [...chain, matrice[x] ? matrice[x][y] : undefined];
 
   if (newChain.length >= matrice.length) return newChain;
 
@@ -42,29 +40,35 @@ export function exploreVector(matrice, currentCoords, direction, chain = []) {
     matrice,
     getNewCoords(currentCoords, direction),
     direction,
-    newChain
+    newChain,
   );
 }
 
 export function searchForWin(matrice) {
+  // console.warn("matrice", matrice);
   let lines = [];
   for (const rowId in matrice) {
     const row = matrice[rowId];
     for (const itemId in row) {
+      // console.warn("itemId", itemId);
       // TODO: find a way to find the line back in a more explicit way (no "0,0_0,1")
       lines = {
-        "0,0_0,1": exploreVector(matrice, [rowId, itemId], [0, 1]),
-        "0,0_1,0": exploreVector(matrice, [rowId, itemId], [1, 0]),
-        "0,0_1,1": exploreVector(matrice, [rowId, itemId], [1, 1])
+        '0,0_0,1': exploreVector(matrice, [rowId, itemId], [0, 1]),
+        '0,0_1,0': exploreVector(matrice, [rowId, itemId], [1, 0]),
+        '0,0_1,1': exploreVector(matrice, [rowId, itemId], [1, 1]),
+        '0,0_1,-1': exploreVector(matrice, [rowId, itemId], [1, -1]),
       };
 
-      return findWinPattern(lines);
+      const winningLine = findWinPattern(lines);
+
+      if (!!winningLine) return winningLine;
     }
   }
 }
 
 export function pickWinner(board) {
   const matrice = buildMatrice(board);
-  const hasWinningPattern = searchForWin(matrice);
-  return "X";
+  const winningLine = searchForWin(matrice);
+
+  return winningLine ? winningLine[1] : undefined;
 }
