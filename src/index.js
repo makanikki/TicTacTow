d0dbf7583e41291873afea2006c7339951de0ae7
Mathieu ROBERT <mathieu@rbtmathieu.fr>
2019-04-10 11:54:00 +0200

    Pick winner & misc

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./styles.css";
-import { pickWinner } from "./services/winnerPicker";
-import { searchForWin } from "./services/plop";
+import './styles.css';
+import { pickWinner } from './services/winnerPicker';
 
-const PLAYERS = ["X", "O"];
+const PLAYERS = ['X', 'O'];
+const BOARD_SIDE_LENGTH = 5;
+
+function generateBoard(boardSideLength) {
+  return [...new Array(Math.pow(boardSideLength, 2))].reduce(
+    (board, _, i) => ({
+      ...board,
+      [i]: undefined,
+    }),
+    {},
+  );
+}
 
 function getNextPlayer(player, players) {
   const currentIndex = players.indexOf(player);
   if (currentIndex + 1 >= players.length) return players[0];
 
   return players[currentIndex + 1];
+}
+
+function getGameStatus(isGameFinished, winner) {
+  return isGameFinished
+    ? winner !== 'Nobody'
+      ? 'Won'
+      : 'Draw'
+    : 'On Progress';
 }
 
 function Square(props) {
@@ -23,29 +41,17 @@ function Square(props) {
 }
 
 function Board(props) {
-  const [currentPlayer, setCurrentPlayer] = React.useState("X");
-  // const [configuration, setConfiguration] = React.useState({
-  //   1: undefined,
-  //   2: undefined,
-  //   3: undefined,
-  //   4: undefined,
-  //   5: undefined,
-  //   6: undefined,
-  //   7: undefined,
-  //   8: undefined,
-  //   9: undefined
-  // });
-  const [board, setBoard] = React.useState(new Array(9));
-  const [isGameFinished, setIsGameFinished] = React.useState(false);
+  const [currentPlayer, setCurrentPlayer] = React.useState('X');
+  const [board, setBoard] = React.useState(generateBoard(BOARD_SIDE_LENGTH));
 
   React.useEffect(() => {
-    props.onConfigChange(board);
+    props.onBoardChange(board);
   }, [props, board]);
 
   function handleClick(i) {
     setBoard({
       ...board,
-      [i]: currentPlayer
+      [i]: currentPlayer,
     });
     setCurrentPlayer(getNextPlayer(currentPlayer, PLAYERS));
   }
@@ -55,31 +61,31 @@ function Board(props) {
     return (
       <Square
         value={board[i] || null}
-        onClick={isEmpty ? () => handleClick(i) : null}
+        onClick={isEmpty && !props.isFinnished ? () => handleClick(i) : null}
       />
     );
   }
 
   const status = `Next player: ${currentPlayer}`;
 
+  const renderBoard = boardSideLength => {
+    const boardRows = [];
+    let row = [];
+
+    for (let rowId = 0; rowId < boardSideLength; ++rowId) {
+      row = [...new Array(boardSideLength)].map((item, index) =>
+        renderSquare(rowId * boardSideLength + index),
+      );
+      boardRows.push(<div className="board-row">{row}</div>);
+    }
+
+    return boardRows;
+  };
+
   return (
     <div>
       <div className="status">{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {renderBoard(BOARD_SIDE_LENGTH)}
     </div>
   );
 }
@@ -88,27 +94,31 @@ function Game() {
   const [isGameFinished, setIsGameFinished] = React.useState(false);
   const [winner, setWinner] = React.useState(undefined);
 
-  function onConfigChange(configuration) {
-    const emptyCell = Object.keys(configuration).filter(
-      config => configuration[config] === undefined
+  function onBoardChange(board) {
+    const emptyCells = Object.keys(board).filter(
+      config => board[config] === undefined,
     );
-    setIsGameFinished(emptyCell.length === 0);
 
-    // if (isGameFinished) setWinner(pickWinner(configuration));
+    const potentialWinner = pickWinner(board);
+
+    setIsGameFinished(!!potentialWinner || emptyCells.length === 0);
+
+    if (isGameFinished) setWinner(potentialWinner || 'Nobody');
   }
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board onConfigChange={onConfigChange} />
+        <Board onBoardChange={onBoardChange} isFinnished={isGameFinished} />
       </div>
       <div className="game-info">
         <div>
-          Status: <i>{isGameFinished ? "Over" : "In progress"}</i>
+          Status: <i>{getGameStatus(isGameFinished, winner)}</i>
         </div>
         <div>
           Winner: <i>{winner}</i>
         </div>
+        <button onClick={() => window.location.reload()}>Restart</button>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
@@ -117,4 +127,4 @@ function Game() {
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById('root'));
